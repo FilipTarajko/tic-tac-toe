e2ba72d50a4cfca28b9b39cbef02deaebb44b91f
Filip Tarajko <filiptarajko@gmail.com>
2021-05-25 00:29:07 +0200

    generate buttons with js, change visuals

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("abc")
+const contentGrid = document.querySelector("#grid");
+let textArray = new Array("X","O","X","O","X","O","X","O","X");
+
+for (let i=0; i<9; i++)
+{
+    let newButton = document.createElement("button");
+    newButton.textContent = textArray[i];
+    newButton.classList.add("cell");
+    contentGrid.appendChild(newButton);
+}
